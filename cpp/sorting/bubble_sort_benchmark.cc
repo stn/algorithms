ff916ff682bf52081e89840f04fa4434d4f78283
Akira Ishino <akira.ishino@gmail.com>
2021-06-23 20:55:56 +0900

    Add more benchmarks of bubble sort

--- cpp/sorting/bubble_sort_benchmark.cc
+++ cpp/sorting/bubble_sort_benchmark.cc
@@ -1,3 +1,5 @@
+#include <random>
+
 #include <benchmark/benchmark.h>
 
 #include "bubble_sort.h"
@@ -22,7 +24,54 @@ static void BM_BubbleSortAsc(benchmark::State& state) {
     state.SetComplexityN(n);
 }
 
+static void BM_BubbleSortDesc(benchmark::State& state) {
+    const int n = state.range(0);
+
+    for (auto _ : state) {
+        state.PauseTiming();
+        int* arr = descending_array(n);
+        state.ResumeTiming();
+
+        bubble_sort(arr, n);
+
+        state.PauseTiming();
+        delete arr;
+        state.ResumeTiming();
+    }
+    //state.SetItemsProcessed(int64_t(state.iterations()) * int64_t(n));
+    state.SetComplexityN(n);
+}
+
+static void BM_BubbleSortRandom(benchmark::State& state) {
+    const int n = state.range(0);
+
+    std::mt19937 mt;
+    mt.seed(mt.default_seed);
+
+    for (auto _ : state) {
+        state.PauseTiming();
+        int* arr = random_array(n, mt);
+        state.ResumeTiming();
+
+        bubble_sort(arr, n);
+
+        state.PauseTiming();
+        delete arr;
+        state.ResumeTiming();
+    }
+    //state.SetItemsProcessed(int64_t(state.iterations()) * int64_t(n));
+    state.SetComplexityN(n);
+}
+
 BENCHMARK(BM_BubbleSortAsc)
+    ->RangeMultiplier(2)->Range(8, 8<<12)
+    ->Complexity([](benchmark::IterationCount n)->double{return n * n;});
+
+BENCHMARK(BM_BubbleSortDesc)
+    ->RangeMultiplier(2)->Range(8, 8<<12)
+    ->Complexity([](benchmark::IterationCount n)->double{return n * n;});
+
+BENCHMARK(BM_BubbleSortRandom)
     ->RangeMultiplier(2)->Range(8, 8<<12)
     ->Complexity([](benchmark::IterationCount n)->double{return n * n;});
 
