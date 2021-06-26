f062b707113a488d8705e2cc8aaab0e2dfc2f11e
Akira Ishino <akira.ishino@gmail.com>
2021-06-26 23:42:49 +0900

    Change bubble sort implementation

--- cpp/sorting/bubble_sort.cc
+++ cpp/sorting/bubble_sort.cc
@@ -2,11 +2,11 @@
 
 void bubble_sort(int *arr, int n) {
     for (int i = 0; i < n - 1; ++i) {
-        for (int j = 0; j < n - i - 1; ++j) {
-            if (arr[j] > arr[j + 1]) {
+        for (int j = n - 1; j > i; --j) {
+            if (arr[j - 1] > arr[j]) {
                 int tmp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = tmp;
+                arr[j] = arr[j - 1];
+                arr[j - 1] = tmp;
             }
         }
     }
