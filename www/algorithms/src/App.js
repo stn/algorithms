32818af912b1acbf85eeb70bcda044cbea5aad81
Akira Ishino <akira.ishino@gmail.com>
2021-07-25 18:39:23 +0900

    www/algorithm

---
+++ www/algorithms/src/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li>Bubble Sort</li>
+        <li>Insertion Sort</li>
+        <li>Quick Sort</li>
+        <li>Selection Sort</li>
+        <li>Shaker Sort</li>
+        <li>Shell Sort</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
